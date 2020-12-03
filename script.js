327260246e933ce4c2cc9471796e051c6d1df517
ivanfarina08 <ivan.farina1@gmail.com>
2020-12-03 09:47:28 -0300

    Jogo completo

--- script.js
+++ script.js
@@ -46,6 +46,13 @@ function iniciarJogo(){
     if(snake [0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake [0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i=1; i< snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -75,4 +82,4 @@ function iniciarJogo(){
 
 }
 
-let jogo = setInterval(iniciarJogo,500);
+let jogo = setInterval(iniciarJogo,150);
